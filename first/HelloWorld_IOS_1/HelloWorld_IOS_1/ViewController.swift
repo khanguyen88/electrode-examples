db1092004f70a2861dfb70471589c77a26a190e1
Kha Nguyen <khanguyen@Khas-MacBook-Pro.local>
2018-03-22 22:21:37 +0700

    standalon hello world apps

---
+++ first/HelloWorld_IOS_1/HelloWorld_IOS_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld_IOS_1
+//
+//  Created by Kha Nguyen on 3/22/18.
+//  Copyright © 2018 Coderschool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
